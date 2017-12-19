9317f6f8d5a4c4a9f986c33ac3f6cb8175070609
zhousong <zhousong@jd.com>
2017-12-19 19:51:33 +0800

    问题9

--- src/main/java/leetcode/Question9.java
+++ src/main/java/leetcode/Question9.java
@@ -1,5 +1,17 @@
 package leetcode;
 
+/**
+ * <p>
+ * Determine whether an integer is a palindrome. Do this without extra space.
+ * 
+ * @author zhousong
+ *
+ */
 public class Question9 {
 
+	public boolean isPalindrome(int x) {
+        for(int i = 0; i <= x / 2; i++) {
+        	
+        }
+    }
 }
